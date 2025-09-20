5203475d14fcd7e3dac8267c39566cbd1a13e625
basavaraju <basava9240@gamil.com>
2025-09-20 07:26:03 +0530

    devcom paltform file changed name

---
+++ add.java
@@ -0,0 +1,11 @@
+package mergeconfict;
+
+public class add {
+    public static void main( String[]args){
+        int a,b,c;
+        a=20;
+        b=10;
+       c=a+b;
+       System.out.println(c);
+    }
+}
